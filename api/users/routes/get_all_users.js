01f306141b0da7f5e9b39a2e54e1199d717a6059
Fady Abdelmalik <fady@auth0.com>
2021-02-21 15:37:03 -0500

    test

--- api/users/routes/get_all_users.js
+++ api/users/routes/get_all_users.js
@@ -1,11 +1,22 @@
-const glob = require('glob');
-const xx = require('path-to-regexp');
+// const glob = require("glob");
+// const xx = require("path-to-regexp");
+const { promisify } = require("util");
+const { exec: execCallback } = require("child_process");
+const exec = promisify(execCallback);
+
+(async () => {
+  console.log(
+    await exec(
+      "aws s3 cp /build/api/users/routes/patch_user.js s3://lambda-builds-test/patch_user.js"
+    )
+  );
+})();
 
 module.exports = {
-  method: 'GET',
-  path: '/api/users',
+  method: "GET",
+  path: "/api/users",
   handler: async (context) => {
-    console.log('Getting all users....');
+    console.log("Getting all users....");
     return;
   },
 };
