f6d7a8ed15b239427a28f99a27ac962fc47b0dab
Fady Abdelmalik <fady@auth0.com>
2021-02-22 19:27:38 -0500

    remove sdk aws

--- api/users/routes/get_all_users.js
+++ api/users/routes/get_all_users.js
@@ -1,16 +1,5 @@
-// const glob = require("glob");
-// const xx = require("path-to-regexp");
-const { promisify } = require("util");
-const { exec: execCallback } = require("child_process");
-const exec = promisify(execCallback);
-
-(async () => {
-  console.log(
-    await exec(
-      "aws s3 cp /build/api/users/routes/patch_user.js s3://lambda-builds-test/patch_user.js"
-    )
-  );
-})();
+const glob = require("glob");
+const xx = require("path-to-regexp");
 
 module.exports = {
   method: "GET",
